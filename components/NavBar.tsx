dc6619d433f2a1d3ffeea4ecf99287bae8a6dc5c
Muhammad Faridz <faridzmuhamad679@gmail.com>
2024-06-10 23:55:04 +0700

    perbaikan nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -135,7 +135,7 @@ export default function NavBar() {
                 }}
                 transition={{ duration: 0.3 }}
               >
-                contact
+                Contact
               </motion.p>
             </Link>
           </div>
