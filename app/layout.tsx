15505066e2085b34061333f800a21a91d76f3036
Muhammad Faridz <faridzmuhamad679@gmail.com>
2024-07-23 17:20:28 +0700

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,9 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Muhammad Faridz",
+  title: "Muhammad Faridz | fardz2",
   description:
-    "I am Muhammad Faridz, a student pursuing a Bachelor's degree in Informatics at Telkom University Bandung, specializing in Front-End and Back-End Web Development, Front-End Mobile Development, and Machine Learning. Choosing to major in informatics was a logical decision for me, given my background in vocational school. I have a particular interest in programming, focusing on Web and Mobile Development. My goal is to improve my computer skills and effectively tackle emerging technological challenges. I began my educational journey at SMK Telkom Banjarbaru in Kalimantan Selatan, where I studied from July 2018 to May 2021 and received the OPES (One Pipe Education System) scholarship from the Telkom Foundation. Currently, I am continuing my studies at Telkom University in Bandung, where I have been enrolled since September 2021.",
+    "I am Muhammad Faridz, a student pursuing a Bachelor's degree in Informatics at Telkom University Bandung, specializing in Front-End and Back-End Web Development, Front-End Mobile Development, and Machine Learning. Choosing to major in informatics was a logical decision for me, given my background in vocational school. I have a particular interest in programming, focusing on Web and Mobile Development. My goal is to improve my computer skills and effectively tackle emerging technological challenges. I began my educational journey at SMK Telkom Banjarbaru in Kalimantan Selatan, where I studied from July 2018 to May 2021 and received the OPES (One Pipe Education System) scholarship from the Telkom Foundation. Currently, I am continuing my studies at Telkom University in Bandung, where I have been enrolled since September 2021. fardz2",
 };
 
 export default function RootLayout({
