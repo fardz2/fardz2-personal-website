06ad9cc05db5912a5da1c2e885a154363224ff9c
Muhammad Faridz <faridzmuhamad679@gmail.com>
2024-08-11 07:14:33 +0800

    Remove .env file from repository

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-
-    return config;
-  },
   images: {
     remotePatterns: [
       {
